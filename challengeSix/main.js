743f2eb97cb7fe51dd3e2d462ff5986fa502bd01
Rangana Maddumahewa <rbasehewa@gmail.com>
2019-10-02 09:05:09 +0930

    replaced kebabstring function and factorial function

--- challengeSix/main.js
+++ challengeSix/main.js
@@ -1,18 +1,27 @@
 
-function isEven(x) {
-	if (x % 2 === 0) {
+function isEven(num) {
+	if (num % 2 === 0) {
 		return true;
 	} else {
 		return false;
 	}
 }
 
-var factorial =1;
-
-function findFactorial(x){
-	for(i=1; i <= x; i++){
-		factorial = factorial * i;
+function factorial(num) {
+	// define the result variable
+	var result = 1;
+	// calculate factorial and store value in result 
+	for (var i = 2; i <= num; i++) {
+		result *= i;
 	}
+	// return the result variable
+	return result;
+}
 
-	return factorial;
+function kebabToSnake(stringvalue){
+
+	// replace all '-' dashes to '_' underscore
+	var newStr = stringvalue.replace(/-/g, '_');
+	// return stringvalue
+	return newStr;
 }
