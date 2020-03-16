ef391bd0c494023913600320d42b1215520aa43f
Ryan Maddumahewa <rbasehewa@gmail.com>
2020-03-16 12:52:58 +1100

    adding new files

---
+++ challengeEight/index.js
@@ -0,0 +1,25 @@
+import { arrayExpression } from "babel-types";
+
+console.log("connected")
+
+
+function printReverseArray(arr){
+    for(var i=arr.length - 1 ; i >= 0; i--){
+        console.log(arr[i]);
+    }
+}
+
+printReverseArray([2,3,4,5]);
+
+function isUniform(array){
+
+    var first = arr[0];
+
+    for(var i=array.length - 1 ; i >=0; i--){
+
+        
+    } 
+
+}
+
+isUniform([1,1,1,1]);
