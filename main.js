41392eca165253c6b5e27cf5f61bcc569a59045f
rbasehewa <rbasehewa@gmail.com>
2021-08-27 14:31:05 +1000

    adding sample js

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 var techies = ["Html","CSS","Javascript","DOM"];
 
+function createMenu( items ) {
+    //let's create a new element for containing the menu
+    var menu = document.createElement("ul");
+    
+    //iterate over the items and create one LI for each item
+    items.forEach(function(element, index, array){
+      var menuItem = document.createElement("li");
+      var textNode = document.createTextNode(element);
+      menuItem.appendChild(textNode);
+      
+      menu.appendChild(menuItem);
+    });
+    
+    document.body.appendChild(menu);
+  }
+  
+  createMenu(technologies);
